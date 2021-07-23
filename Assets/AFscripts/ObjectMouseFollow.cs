b26221d1c3b24f60d44818e5194cc04c3075e2ab
Aaron <A.fuser04@gmail.com>
2021-07-23 14:24:16 +1000

    Menu implementation and extra folders

---
+++ Assets/AFscripts/ObjectMouseFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMouseFollow : MonoBehaviour
+{
+    public Transform _movingObj;
+    public float _movementSpeed;
+    public bool _canMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_canMove == true)
+            _movingObj.transform.position += _movingObj.transform.forward * Time.deltaTime * _movementSpeed;
+        else if (_canMove == false)
+            _movingObj.transform.position = _movingObj.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _canMove = false;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        _canMove = true;
+    }
+}
---
+++ Assets/AFscripts/speedChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedChange : MonoBehaviour
+{
+    public float _newSpeed;
+    public ObjectMouseFollow _targetSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        _targetSpeed._movementSpeed = _newSpeed;
+    }
+}
