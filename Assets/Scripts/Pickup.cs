a9a85334cc58bd37ce572f9b5b86cd8abfce4fc1
Ruben <madcrafty@icloud.com>
2021-07-30 11:00:52 +1000

    Updated pickup, added supplybox function and repair zone tmp

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -11,7 +11,7 @@ public class Pickup : MonoBehaviour
     private RaycastHit _info;
     private Ray _ray;
     private bool _hold = false;
-    private Transform _objectHeld;
+    private GameObject _objectHeld;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,56 +26,71 @@ public class Pickup : MonoBehaviour
             {
                 if (value.interaction is TapInteraction)
                 {
-                    _objectHeld.SetParent(null);
-                    if (_objectHeld.GetComponent<Rigidbody>() == null)
-                    {
-                        _objectHeld.gameObject.AddComponent<Rigidbody>();
-                    }
-                    else if (_objectHeld.GetComponent<Rigidbody>().isKinematic == true)
-                    {
-                        _objectHeld.GetComponent<Rigidbody>().isKinematic = false;
-                    }
-                    _objectHeld = null;
-                    _hold = !_hold;
+                    Drop();
                 }
                 else if (value.interaction is HoldInteraction)
                 {
-                    _objectHeld.SetParent(null);
-                    if (_objectHeld.GetComponent<Rigidbody>() == null)
-                    {
-                        _objectHeld.gameObject.AddComponent<Rigidbody>();
-                    }
-                    else if (_objectHeld.GetComponent<Rigidbody>().isKinematic == true)
-                    {
-                        _objectHeld.GetComponent<Rigidbody>().isKinematic = false;
-                    }
-                    _objectHeld.GetComponent<Rigidbody>().AddForce(transform.forward * _ThrowPower, ForceMode.VelocityChange);
-                    _objectHeld = null;
-                    _hold = !_hold;
+                    Throw();
                 }
             }
             else
             {
                 if (_info.collider != null && _info.collider.name != name)
                 {
-                    _objectHeld = _info.transform;
-                    if (_objectHeld.GetComponent<Rigidbody>() != null && _objectHeld.GetComponent<Rigidbody>().isKinematic == false)
-                    {
-                        _objectHeld.GetComponent<Rigidbody>().isKinematic = true;
-                    }
-                    _objectHeld.SetParent(null);
-                    _objectHeld.SetParent(transform);
-                    _objectHeld.localPosition = Vector3.forward* 1.1f + Vector3.up * 0.75f;
-                    _hold = !_hold;
+                    Grab(_info.transform.gameObject);
                 }
             }
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    void Grab(GameObject Suplies)
     {
-        
+        _objectHeld = Suplies;
+        if (_objectHeld.GetComponent<SupplyCrate>() != null && _objectHeld.GetComponent<SupplyCrate>().Contents != null)
+        {
+            //Grab(_objectHeld.GetComponent<SupplyCrate>().Contents);
+            _objectHeld = Instantiate(_objectHeld.GetComponent<SupplyCrate>().Contents);
+        }
+        if (_objectHeld.GetComponent<Rigidbody>() != null && _objectHeld.GetComponent<Rigidbody>().isKinematic == false)
+        {
+            _objectHeld.GetComponent<Rigidbody>().isKinematic = true;
+        }
+        _objectHeld.transform.SetParent(null);
+        _objectHeld.transform.SetParent(transform);
+        _objectHeld.transform.localPosition = Vector3.forward * 1.1f + Vector3.up * 0.75f;
+        _hold = !_hold;
+    }
+    void Drop()
+    {
+        _objectHeld.transform.SetParent(null);
+        if (_objectHeld.GetComponent<Rigidbody>() == null)
+        {
+            _objectHeld.gameObject.AddComponent<Rigidbody>();
+        }
+        else if (_objectHeld.GetComponent<Rigidbody>().isKinematic == true)
+        {
+            _objectHeld.GetComponent<Rigidbody>().isKinematic = false;
+        }
+        _objectHeld = null;
+        _hold = !_hold;
+    }
+    void Throw()
+    {
+        _objectHeld.transform.SetParent(null);
+        if (_objectHeld.GetComponent<Rigidbody>() == null)
+        {
+            _objectHeld.gameObject.AddComponent<Rigidbody>();
+        }
+        else if (_objectHeld.GetComponent<Rigidbody>().isKinematic == true)
+        {
+            _objectHeld.GetComponent<Rigidbody>().isKinematic = false;
+        }
+        _objectHeld.GetComponent<Rigidbody>().AddForce(transform.forward * _ThrowPower, ForceMode.VelocityChange);
+        _objectHeld = null;
+        _hold = !_hold;
+    }
+    public GameObject GetHeldObject()
+    {
+        return _objectHeld;
     }
     public void OnDrawGizmos()
     {
---
+++ Assets/Scripts/RepairZone.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairZone : MonoBehaviour
+{
+    public GameObject SuppliesToFix;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetComponent<Pickup>().GetHeldObject().name == SuppliesToFix.name)
+            {
+                Debug.Log("repaired");
+            }
+            
+        }
+    }
+}
